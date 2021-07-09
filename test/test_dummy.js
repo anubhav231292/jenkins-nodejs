abf58064cc58aa11d510c2745f0ae83c335879e5
agarg <anubhav.garg231292@gmail.com>
2021-07-09 14:53:08 +0530

    correct test case

--- test/test_dummy.js
+++ test/test_dummy.js
@@ -3,7 +3,7 @@ const assert = require("assert");
 describe("dummy test case", () => {
 
   it("1 == 1", () => {
-    assert.strictEqual(1, 2, "error in 1 == 1 dummy test case.");
+    assert.strictEqual(1, 1, "error in 1 == 1 dummy test case.");
   });
 
 });
